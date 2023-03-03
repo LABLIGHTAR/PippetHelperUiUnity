e42cfcb8e91fe9c34d317f618f408061e71d290a
Nik Stires <nik.stires@gmail.com>
2023-03-03 11:47:12 -0800

    finished tutorial v1:

---
+++ Assets/TutorialController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public List<TutorialPanelController> panels;
+
+    private int currentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPanel = 0;
+        panels[currentPanel].gameObject.SetActive(true);
+        SessionState.FormActive = true;
+
+        foreach(TutorialPanelController panel in panels)
+        {
+            panel.nextButton.onClick.AddListener(delegate
+            {
+                if (currentPanel < panels.Count - 1)
+                {
+                    panels[currentPanel].gameObject.SetActive(false);
+                    currentPanel++;
+                    panels[currentPanel].gameObject.SetActive(true);
+                }
+                else
+                {
+                    this.gameObject.SetActive(false);
+                    SessionState.FormActive = false;
+                }
+            });
+
+            panel.skipButton.onClick.AddListener(delegate { 
+                this.gameObject.SetActive(false); 
+                SessionState.FormActive = false;
+            });
+        }
+    }
+}
---
+++ Assets/TutorialPanelController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPanelController : MonoBehaviour
+{
+    public Button skipButton;
+    public Button nextButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
