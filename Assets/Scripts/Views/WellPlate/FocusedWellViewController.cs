629726873dc48fa57d3b24df4921d70561be8715
Nik Stires <nik.stires@gmail.com>
2023-04-28 14:08:39 -0700

    added all samples from all steps to focused well view

--- Assets/Scripts/Views/WellPlate/FocusedWellViewController.cs
+++ Assets/Scripts/Views/WellPlate/FocusedWellViewController.cs
@@ -13,12 +13,23 @@ public class FocusedWellViewController : MonoBehaviour
     public TextMeshProUGUI wellGroupText;
 
     public GameObject SampleItemPrefab;
-    public Transform ScrollViewContent;
+    public RectTransform ScrollViewContent;
 
     // Start is called before the first frame update
     void Start()
     {
         SessionState.focusedWellStream.Subscribe(well => UpdateVisualState(well)).AddTo(this);
+    }
+
+    void Update()
+    {
+        if(ScrollViewContent.childCount > 4)
+        {
+            if(ScrollViewContent.localPosition.y < ScrollViewContent.rect.height/2)
+                ScrollViewContent.localPosition = new Vector3(ScrollViewContent.localPosition.x, ScrollViewContent.localPosition.y + 0.15f, ScrollViewContent.localPosition.z);
+            else
+                ScrollViewContent.localPosition = new Vector3(ScrollViewContent.localPosition.x, 0f, ScrollViewContent.localPosition.z);
+        }
     }
 
     void UpdateVisualState(Well well)
@@ -27,11 +38,14 @@ public class FocusedWellViewController : MonoBehaviour
 
         float wellVolume = 0f;
         
-        foreach(LabAction action in SessionState.CurrentStep.GetActionsWithTargetWell(well).Where(action => action.type == LabAction.ActionType.pipette))
+        foreach(Step step in SessionState.Steps)
         {
-            wellVolume += action.source.volume;
-            var newSampleItem = Instantiate(SampleItemPrefab, ScrollViewContent);
-            newSampleItem.GetComponent<FocusedSampleItemViewController>().InitItem(action);
+            foreach (LabAction action in step.GetActionsWithTargetWell(well).Where(action => action.type == LabAction.ActionType.pipette))
+            {
+                wellVolume += action.source.volume;
+                var newSampleItem = Instantiate(SampleItemPrefab, ScrollViewContent);
+                newSampleItem.GetComponent<FocusedSampleItemViewController>().InitItem(action);
+            }
         }
 
         //update well display
